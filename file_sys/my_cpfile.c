c96234879d1b6c2bced9a7b41b05fc8d26702286
u24-lbai <zkong886@gmail.com>
2025-07-18 13:56:54 +0800

    封装函数

--- file_sys/my_cpfile.c
+++ file_sys/my_cpfile.c
@@ -1,11 +1,17 @@
 #include "head.h"
 
+int copy_file(const char *src, const char *dest);
 int main(int argc, char **argv)
 {
     // ./cp file1 file2
     ARGS_CHECK(argc, 3);
-    int fdr = open(argv[1], O_RDONLY);
-    int fdw = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0666);
+    copy_file(argv[1], argv[2]);
+    return 0;
+}
+
+int copy_file(const char *src, const char *dest) {
+    int fdr = open(src, O_RDONLY);
+    int fdw = open(dest, O_WRONLY | O_CREAT | O_TRUNC, 0666);
 
     char buf[1024] = "";
     while (1) {
@@ -23,4 +29,3 @@ int main(int argc, char **argv)
     close(fdw);
     return 0;
 }
-
