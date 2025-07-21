f12e16012165b7806b1a7326526e5ac926c2d7d8
u24-lbai <zkong886@gmail.com>
2025-07-21 20:02:09 +0800

    测试消息队列

---
+++ ipc/msgrcv.c
@@ -0,0 +1,15 @@
+#include "head.h"
+
+int main(int argc, char **argv)
+{
+    int msgid = msgget(1000, IPC_CREAT | 0600);
+    RET_CHECK(msgid, -1, "msgget");
+
+
+    Msg_t msg;
+    msgrcv(msgid, &msg, sizeof(msg.mtext), 1, 0);
+    printf("type=%ld, text=%s\n", msg.mtype, msg.mtext);
+
+    return 0;
+}
+
---
+++ ipc/msgsnd.c
@@ -0,0 +1,17 @@
+#include "head.h"
+
+int main(int argc, char **argv)
+{
+    ARGS_CHECK(argc, 2);
+    int msgid = msgget(1000, IPC_CREAT | 0600);
+    RET_CHECK(msgid, -1, "msgget");
+
+
+    Msg_t msg;
+    msg.mtype = 1;
+    strcpy(msg.mtext, argv[1]);
+    msgsnd(msgid, &msg, strlen(msg.mtext), 0);
+
+    return 0;
+}
+
