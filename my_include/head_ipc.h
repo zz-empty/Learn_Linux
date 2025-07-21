ea9c4c818a6095f6e2a780479a3da0e2973b6bb0
dice <drawouter@163.com>
2025-07-21 15:13:12 +0800

    添加ipc头文件

--- my_include/head_ipc.h
+++ my_include/head_ipc.h
@@ -1,14 +1,15 @@
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/ipc.h>
+#include <sys/mman.h>
+#include <sys/select.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <time.h>
 #include <unistd.h>
-#include <fcntl.h>
-#include <sys/mman.h>
-#include <sys/wait.h>
-#include <sys/select.h>
 
 // 检查命令行参数个数是否正确
 #define ARGS_CHECK(argc, num)                                                  \
