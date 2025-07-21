edcb2e3e1081912cd75bbe3e776124bcb8aced33
u24-lbai <zkong886@gmail.com>
2025-07-21 16:54:57 +0800

    父子进程同时操作共享内存

---
+++ ipc/shm_addbignum.c
@@ -0,0 +1,32 @@
+#include "head.h"
+
+int main(int argc, char **argv)
+{
+    ARGS_CHECK(argc, 2);
+    int NUM = atoi(argv[1]);
+
+    int shmid = shmget(IPC_PRIVATE, 4096, IPC_CREAT | 0600);
+    char*p = (char*)shmat(shmid, NULL, 0);
+
+    p[0] = 0;
+
+    if (!fork()) {
+        // 子进程
+        for (int i = 0; i < NUM; ++i) {
+            ++p[0];
+        }
+        _exit(0);
+    } else {
+        // 父进程
+        for (int i = 0; i < NUM; ++i) {
+            ++p[0];
+        }
+        wait(NULL);
+        printf("Total = %d\n", p[0]);
+        shmdt(p);
+        exit(0);
+    }
+
+    return 0;
+}
+
