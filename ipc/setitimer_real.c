a8b3319ebe19111eeac00e58a7532d170f4faa0f
u24-lbai <zkong886@gmail.com>
2025-07-23 07:20:54 +0800

    设置一个真实计时器

---
+++ ipc/setitimer_real.c
@@ -0,0 +1,24 @@
+#include "head.h"
+
+void sigFunc(int signum) {
+    time_t now;
+    time(&now);
+    printf("%s\n", ctime(&now));
+}
+
+int main()
+{
+    signal(SIGALRM, sigFunc);
+
+    struct itimerval timer;
+    timer.it_interval.tv_sec = 1;   // 每次发送信号的间隔
+    timer.it_interval.tv_usec = 0;
+    timer.it_value.tv_sec = 3;  // 第一次发送的信号的时间
+    timer.it_value.tv_usec = 0;
+
+    setitimer(ITIMER_REAL, &timer, NULL);
+    sigFunc(0); // 打印当前时间
+    while(1);
+    return 0;
+}
+
