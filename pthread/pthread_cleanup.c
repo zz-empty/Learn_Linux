96cfeb5c6c84499281c57cb3844d680f0b6ced66
zk <drawouter@163.com>
2025-07-27 12:59:55 +0800

    调整

--- pthread/pthread_cleanup.c
+++ pthread/pthread_cleanup.c
@@ -1,12 +1,22 @@
 #include "head.h"
 
+void cleanfunc(void *p) {
+    printf("cleanfunc\n");
+    if (p) {
+        free(p);
+        p = NULL;
+    }
+}
+
 void *threadfunc(void *val) {
   printf("I am child_thread func! \n");
   char *p = (char *)malloc(128);
+  pthread_cleanup_push(cleanfunc, p);
   strcpy(p, "this is child!");
   printf("p = %s\n", p);
   free(p);
   printf("success free!\n");
+  pthread_cleanup_pop(1);
   pthread_exit(NULL);
 }
 
