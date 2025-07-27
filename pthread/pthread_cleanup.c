84f2712787ac179c5a164c11f4403319425f40da
dice <drawouter@163.com>
2025-07-27 12:52:26 +0800

    线程终止函数

---
+++ pthread/pthread_cleanup.c
@@ -0,0 +1,24 @@
+#include "head.h"
+
+void *threadfunc(void *val) {
+  printf("I am child_thread func! \n");
+  char *p = (char *)malloc(10);
+  strcpy(p, "this is child!");
+  printf("p = %s\n", p);
+  printf("bye!\n");
+  free(p);
+  pthread_exit(NULL);
+}
+
+int main() {
+  pthread_t thid;
+  int ret;
+
+  ret = pthread_create(&thid, NULL, threadfunc, NULL);
+  PTHREAD_RET_CHECK(ret, "pthread_create");
+  printf("I am main_thread func!\n");
+
+  pthread_cancel(thid);
+  pthread_join(thid, NULL);
+  return 0;
+}
