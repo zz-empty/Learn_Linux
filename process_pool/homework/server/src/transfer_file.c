aa22b67ff49262c660e208976ec5c25cb3db6795
u24-lbai <zkong886@gmail.com>
2025-08-09 18:42:24 +0800

    读完文件后，要通知客户端

--- process_pool/homework/server/src/transfer_file.c
+++ process_pool/homework/server/src/transfer_file.c
@@ -11,21 +11,26 @@ typedef struct {
 int transferFile(int clientFd) {
     const char *filename = "file1";
     int ret = 0;
+    Data_t data = {};
 
     // 先传输文件名
-    Data_t data = {};
     data.len = strlen(filename);
     strncpy(data.data, filename, DATA_SIZE);
     ret = send(clientFd, &data, sizeof(int) + data.len, 0);
-    RET_CHECK(ret, -1, "send");
+    RET_CHECK(ret, -1, "send_filename");
+    printf("--[Worker] send filename [%s]\n", filename);
+
 
     // 再传输文件大小
     struct stat st = {};
     stat(filename, &st);
     data.len = st.st_size;
     ret = send(clientFd, &data.len, sizeof(int), 0);
+    RET_CHECK(ret, -1, "send_filesize");
+    printf("--[Worker] send filesize [%ld]\n", st.st_size);
     
-    printf("[info] ready send, filename=%s, filesize=%ld\n", filename, st.st_size);
+    printf("--[Worker] ready send, filename=%s, filesize=%ld\n", filename, st.st_size);
+
 
     // 再传输文件内容
     int fd = open(filename, O_RDONLY);
@@ -35,13 +40,15 @@ int transferFile(int clientFd) {
         data.len = read(fd, data.data, DATA_SIZE);
         RET_CHECK(data.len, -1, "read");
         if (0 == data.len) {
-            // 已读完文件
+            // 已读完文件, 通知客户端
+            printf("--[Worker] file read over!\n");
+            send(clientFd, &data, sizeof(data.len), 0);
             break;
         }
 
         // 发送
         ret = send(clientFd, &data, sizeof(int) + data.len, 0);
-        RET_CHECK(ret, -1, "send");
+        RET_CHECK(ret, -1, "send_filecontent");
     }
 
     close(fd);
