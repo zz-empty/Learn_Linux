7c10b56aded58ff4131d91cb36d23332f8d8fb95
u24-lbai <zkong886@gmail.com>
2025-08-09 15:01:39 +0800

    添加任务结构体

--- process_pool/homework/server/src/pool.h
+++ process_pool/homework/server/src/pool.h
@@ -17,7 +17,18 @@ typedef struct {
     Worker_t *workers;   // 工作进程组
 } Pool_t;
 
+// 任务结构
+typedef struct {
+    short exitFlag;
+    int client_fd;
+} Task_t;
+
+// 进程池
 int initPool(Pool_t *pool, Config_t cfg);
 void destroyPool(Pool_t *pool);
+
+// 分配任务&接收任务
+int sendFd(int pipeFd, Task_t *task);
+int recvFd(int pipeFd, Task_t *task);
 
 #endif
