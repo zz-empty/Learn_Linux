77bb53766312b49bded8f07f15208b74d07e37b1
u24-lbai <zkong886@gmail.com>
2025-08-09 16:59:57 +0800

    添加退出信号处理逻辑

--- process_pool/homework/server/src/main.c
+++ process_pool/homework/server/src/main.c
@@ -1,6 +1,28 @@
 #include "head.h"
 #include "pool.h"
 
+#define WAIT_ARRAY_SIZE 10
+
+// 信号通知的管道
+int exitPipe[2];
+
+// 信号处理函数
+void sigFunc(int signum) {
+    printf("[signal] %d is comming...\n", signum);
+    write(exitPipe[1], "1", 1);
+}
+
+// 添加描述符到epoll中
+static int epollAdd(int epfd, int newfd) {
+    struct epoll_event event = {};
+    event.events = EPOLLIN;
+    event.data.fd = newfd;
+
+    int ret = epoll_ctl(epfd, EPOLL_CTL_ADD, newfd, &event);
+    RET_CHECK(ret, -1, "EPOLL_CTL_ADD");
+    return 0;
+}
+
 int main(int argc, char **argv)
 {
     // ./app conf
@@ -11,6 +33,10 @@ int main(int argc, char **argv)
     loadConfig(argv[1], &cfg);
     printConfig(&cfg);
 
+    // 切换到家目录
+    chdir(cfg.home);
+    printf("\n[info] Entry home dir\n");
+
     // 创建进程池
     Pool_t pool;
     initPool(&pool, cfg);
@@ -19,8 +45,79 @@ int main(int argc, char **argv)
     printf("[info] pool init succeed!\n");
 
     // 建立tcp连接
+    int serverFd = tcpListen(cfg);
+    if (-1 == serverFd) {
+        fprintf(stderr, "[error] tcp_socket init failed!\n");
+        exit(EXIT_FAILURE);
+    }
+
     // 建立epoll监听
-    // 退出逻辑
+    int epfd = epoll_create(1);
+    epollAdd(epfd, serverFd);
+    // 注册退出信号
+    pipe(exitPipe);
+    signal(SIGUSR1, sigFunc);
+    epollAdd(epfd, exitPipe[0]);
+
+    // 开始处理各种描述符
+    int readyNum = 0;
+    struct epoll_event evs[WAIT_ARRAY_SIZE] = {};
+    Task_t task = {};
+    char done;
+    printf("[info] Master start...\n");
+    while (1) {
+        readyNum = epoll_wait(epfd, evs, WAIT_ARRAY_SIZE, -1);
+        RET_CHECK(readyNum, -1, "epoll_wait");
+
+        for (int i = 0; i < readyNum; ++i) {
+            if (evs[i].data.fd == exitPipe[0]) {
+                // 退出信号到来, 通知所有子进程退出
+                printf("[warning] Master recv SIGUSR1!!!\n");
+                task.exitFlag = 1;
+                for (int j = 0; j < pool.size; ++j) {
+                    sendFd(pool.workers[j].pipeFd, &task);
+                }
+
+                // 回收所有子进程
+                for (int j = 0; j < pool.size; ++j) {
+                    waitpid(pool.workers[j].pid, NULL, 0);
+                }
+                
+                // 退出
+                printf("[info] all worker exit, Master exit!\n");
+                exit(EXIT_SUCCESS);
+            }
+            else if (evs[i].data.fd == serverFd) {
+                // 新的连接
+                memset(&task, 0, sizeof(Task_t));
+                task.clientFd = accept(serverFd, NULL, 0);
+                RET_CHECK(task.clientFd, -1, "accept");
+                printf("[info] new client fd : %d\n", task.clientFd);
+
+                // 找到空闲的子进程，把任务发给它，再将其设为忙碌
+                for (int j = 0; j < pool.size; ++j) {
+                    if (pool.workers[j].busy == 0) {
+                        printf("[info] worker [%d] start task\n", pool.workers[j].pid);
+                        sendFd(pool.workers[j].pipeFd, &task);
+                        pool.workers[j].busy = 1;
+                        break;
+                    }
+                }
+            }
+            else {
+                // 某个Worker完成任务
+                for (int j = 0; j < pool.size; ++j) {
+                    if (evs[i].data.fd == pool.workers[j].pipeFd) {
+                        // 将子进程设为非忙碌
+                        printf("[info] worker [%d] finish task!\n", pool.workers[j].pid);
+                        read(pool.workers[j].pipeFd, &done, 1);
+                        pool.workers[j].busy = 0;
+                        break;
+                    }
+                }
+            }
+        }
+    }
 
     destroyPool(&pool);
     freeConfig(&cfg);
