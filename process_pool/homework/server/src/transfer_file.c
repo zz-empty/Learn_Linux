cdfbce63afc6f31053f6e4b581023d5d2c732833
u24-lbai <zkong886@gmail.com>
2025-08-09 17:02:32 +0800

    使用自定义协议传输文件

---
+++ process_pool/homework/server/src/transfer_file.c
@@ -0,0 +1,49 @@
+#include "head.h"
+#include "pool.h"
+
+#define DATA_SIZE 1024
+
+typedef struct {
+    int len;
+    char data[DATA_SIZE];
+} Data_t;
+
+int transferFile(int clientFd) {
+    const char *filename = "file1";
+    int ret = 0;
+
+    // 先传输文件名
+    Data_t data = {};
+    data.len = strlen(filename);
+    strncpy(data.data, filename, DATA_SIZE);
+    ret = send(clientFd, &data, sizeof(int) + data.len, 0);
+    RET_CHECK(ret, -1, "send");
+
+    // 再传输文件大小
+    struct stat st = {};
+    stat(filename, &st);
+    data.len = st.st_size;
+    ret = send(clientFd, &data.len, sizeof(int), 0);
+    
+    printf("[info] ready send, filename=%s, filesize=%ld\n", filename, st.st_size);
+
+    // 再传输文件内容
+    int fd = open(filename, O_RDONLY);
+
+    while (1) {
+        memset(&data, 0, sizeof(Data_t));
+        data.len = read(fd, data.data, DATA_SIZE);
+        RET_CHECK(data.len, -1, "read");
+        if (0 == data.len) {
+            // 已读完文件
+            break;
+        }
+
+        // 发送
+        ret = send(clientFd, &data, sizeof(int) + data.len, 0);
+        RET_CHECK(ret, -1, "send");
+    }
+
+    close(fd);
+    return 0;
+}
